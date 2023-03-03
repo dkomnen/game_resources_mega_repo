332f7c8dfcfc0721c7b5768ca3fbb0b4538a7e25
KomisarzRyba <antek.olesik@gmail.com>
2023-03-03 17:37:58 -0500

    basic topdown controller

--- templates_project/templates/player_controllers/top_down_controller/top_down_player_controller.gd
+++ templates_project/templates/player_controllers/top_down_controller/top_down_player_controller.gd
@@ -2,17 +2,22 @@ extends CharacterBody2D
 
 @export var movement_speed = 100
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	if Input.is_action_pressed("ui_left"):
-		velocity.x = -movement_speed
+		velocity.x = -1
 	elif Input.is_action_pressed("ui_right"):
-		velocity.x = movement_speed
+		velocity.x = 1
+	else:
+		velocity.x = 0
 	
 	if Input.is_action_pressed("ui_up"):
-		velocity.y = -movement_speed
+		velocity.y = -1
 	elif Input.is_action_pressed("ui_down"):
-		velocity.y = movement_speed
+		velocity.y = 1
+	else:
+		velocity.y = 0
+	
 
-	velocity = velocity.normalized()
+	velocity = velocity.normalized() * movement_speed
 
 	move_and_slide()
