572680d20cbabea0e467e181dbd933c848ea2cb2
KomisarzRyba <antek.olesik@gmail.com>
2023-03-03 17:28:46 -0500

    topdown test scene

--- templates_project/templates/player_controllers/top_down_controller/top_down_player_controller.gd
+++ templates_project/templates/player_controllers/top_down_controller/top_down_player_controller.gd
@@ -1,11 +1,18 @@
-extends Node2D
+extends CharacterBody2D
 
+@export var movement_speed = 100
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _physics_process(_delta):
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = -movement_speed
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x = movement_speed
+	
+	if Input.is_action_pressed("ui_up"):
+		velocity.y = -movement_speed
+	elif Input.is_action_pressed("ui_down"):
+		velocity.y = movement_speed
 
+	velocity = velocity.normalized()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	move_and_slide()
