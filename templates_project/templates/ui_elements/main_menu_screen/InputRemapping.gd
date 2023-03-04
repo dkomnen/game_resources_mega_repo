208290e8c0909ac73e846a4044df1d727005b83b
David <davidkomljenovic93@gmail.com>
2023-03-04 21:15:02 +0000

    Add 'reset to defaults' option in input remapping

--- templates_project/templates/ui_elements/main_menu_screen/InputRemapping.gd
+++ templates_project/templates/ui_elements/main_menu_screen/InputRemapping.gd
@@ -1,9 +1,9 @@
 extends Control
 
 @onready
-var remapable_keys = [
-	"ui_up", "ui_down", "ui_left", "ui_right"
-]
+var remapable_keys = {
+	"ui_up": "Move up", "ui_down": "Move down", "ui_left": "Move left", "ui_right": "Move right", "ui_accept": "Action"
+}
 
 @export
 var remap_box: Resource
@@ -18,11 +18,10 @@ func _ready():
 	for key in remapable_keys:
 		var box = remap_box.instantiate()
 		$ScrollContainer/VBoxContainer.add_child(box)
-		box.get_node("InputMapLabel").text = key
-		box.get_node("RemapButton").text = InputMap.action_get_events(key)[0].as_text()
 		box.remap_box_id = key
 		box.connect("remap_start", _remap_btn_pressed)
 		remap_boxes[key] = box
+	_refresh_ui()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -32,11 +31,31 @@ func _process(delta):
 func _input(event):
 	if event is InputEventKey and event.pressed and accept_next_input:
 		accept_next_input = false
-		remap_boxes[active_remap_id].get_node("RemapButton").text = event.as_text()
+		KeyPersistence.remap_key(active_remap_id, event)
 		KeyPersistence.keymaps[active_remap_id] = event
-		KeyPersistence.save_keymap()
+		KeyPersistence.save_keymap_to_file()
+		_refresh_ui()
+
 
 func _remap_btn_pressed(input_name: String):
 	accept_next_input = true
+	remap_boxes[input_name].get_node("RemapButton").text = "Press any key"
+	for key in remap_boxes:
+		if key != input_name:
+			remap_boxes[key].get_node("RemapButton").disabled = true
 	active_remap_id = input_name
 
+func _refresh_ui():
+	for key in remap_boxes:
+		var box = remap_boxes[key]
+		box.get_node("RemapButton").disabled = false
+		box.get_node("InputMapLabel").text = remapable_keys[key]
+		box.get_node("RemapButton").text = InputMap.action_get_events(key)[0].as_text()
+
+
+
+func _on_reset_controls_button_pressed():
+	InputMap.load_from_project_settings()
+	_refresh_ui()
+	KeyPersistence.load_project_input_map()
+	KeyPersistence.save_keymap_to_file()
--- templates_project/templates/ui_elements/main_menu_screen/key_persistence.gd
+++ templates_project/templates/ui_elements/main_menu_screen/key_persistence.gd
@@ -7,16 +7,17 @@ var keymaps: Dictionary
 func _ready() -> void:
 	# First we create the keymap dictionary on startup with all
 	# the keymap actions we have.
+	load_project_input_map()
+	load_keymap_from_file()
+
+func load_project_input_map():
 	for action in InputMap.get_actions():
 		if InputMap.action_get_events(action).size() != 0:
 			keymaps[action] = InputMap.action_get_events(action)[0]
-			print(InputMap.action_get_events(action)[0].as_text())
-	load_keymap()
 
-
-func load_keymap() -> void:
+func load_keymap_from_file() -> void:
 	if not FileAccess.file_exists(keymaps_path):
-		save_keymap() # There is no save file yet, so let's create one.
+		save_keymap_to_file() # There is no save file yet, so let's create one.
 		return
 	var file = FileAccess.open(keymaps_path, FileAccess.READ)
 	var temp_keymap = file.get_var(true) as Dictionary
@@ -30,11 +31,13 @@ func load_keymap() -> void:
 			keymaps[action] = temp_keymap[action]
 			# Whilst setting the keymap dictionary, we also set the
 			# correct InputMap event
-			InputMap.action_erase_events(action)
-			InputMap.action_add_event(action, keymaps[action])
+			remap_key(action, keymaps[action])
 
+func remap_key(action: String, event: InputEvent) -> void:
+	InputMap.action_erase_events(action)
+	InputMap.action_add_event(action, event)
 
-func save_keymap() -> void:
+func save_keymap_to_file() -> void:
 	# For saving the keymap, we just save the entire dictionary as a var.
 	var file = FileAccess.open(keymaps_path, FileAccess.WRITE)
 	file.store_var(keymaps, true)
